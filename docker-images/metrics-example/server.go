2e4242b8213397f7067d9772cc58fae9777e2822
Ondrej Sika <ondrej@ondrejsika.com>
2020-09-07 11:54:33 +0200

    docker-images/metrics-example - fix format

--- docker-images/metrics-example/server.go
+++ docker-images/metrics-example/server.go
@@ -1,33 +1,34 @@
 package main
 
 import (
-  "net/http"
+	"net/http"
 
-  "github.com/prometheus/client_golang/prometheus"
-  "github.com/prometheus/client_golang/prometheus/promhttp"
-  "log"
-  "fmt"
+	"fmt"
+	"log"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var  counter_requests = prometheus.NewCounter(
-     prometheus.CounterOpts{
-        Namespace: "example",
-        Name:      "requests",
-        Help:      "Number of requests",
-     })
+var counter_requests = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "example",
+		Name:      "requests",
+		Help:      "Number of requests",
+	})
 
 func main() {
-  http.Handle("/metrics", promhttp.Handler())
+	http.Handle("/metrics", promhttp.Handler())
 
-  prometheus.MustRegister(counter_requests)
+	prometheus.MustRegister(counter_requests)
 
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	counter_requests.Add(1)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		counter_requests.Add(1)
 
-	w.WriteHeader(200)
-	w.Write([]byte("OK"))
-})
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
 
-fmt.Println("Server started.")
-  log.Fatal(http.ListenAndServe(":80", nil))
+	fmt.Println("Server started.")
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
